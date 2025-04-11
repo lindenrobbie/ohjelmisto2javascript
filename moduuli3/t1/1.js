08fa9fb7ca4a639b9fe004357e45da03940ca229
robbie <linden.robbie98@gmail.com>
2025-04-11 17:49:25 +0300

    module3 to github

---
+++ moduuli3/t1/1.js
@@ -0,0 +1,11 @@
+
+
+const myListTing = document.querySelector('#target');
+
+myListTing.innerHTML = `<li>First Item</li> <li>Second item</li>  <li>Third item</li>`;
+
+myListTing.ClassList.add('my-list');
+
+
+
+
