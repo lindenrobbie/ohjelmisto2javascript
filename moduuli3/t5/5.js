ff6e5777f7e7a1990ed1276dea45f0a09a789d18
robbie <linden.robbie98@gmail.com>
2025-04-12 16:49:58 +0300

    moduuli 3 tehtävä 5

--- moduuli3/t5/5.js
+++ moduuli3/t5/5.js
@@ -93,3 +93,37 @@ const picArray = [
 ];
 
 // add your code here
+
+'use strict';
+
+const target = document.getElementById('pictures');
+
+for (let i = 0; i < picArray.length; i++) {
+  const article = document.createElement('article');
+  article.classList.add('card');
+
+  const h2 = document.createElement('h2');
+  h2.textContent = picArray[i].title;
+
+  const figure = document.createElement('figure');
+
+  const img = document.createElement('img');
+  img.src = picArray[i].image.medium;
+  img.alt = picArray[i].title;
+
+  const figcaption = document.createElement('figcaption');
+  figcaption.textContent = picArray[i].caption;
+
+  const p = document.createElement('p');
+  p.textContent = picArray[i].description;
+
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+
+  article.appendChild(h2);
+  article.appendChild(figure);
+  article.appendChild(p);
+
+  target.appendChild(article);
+
+}
