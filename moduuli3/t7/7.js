d1421b11acc1dafc1375852d135b84971f31dcc1
robbie <linden.robbie98@gmail.com>
2025-04-16 17:11:26 +0300

    moduuli 3 tehtävä 7

---
+++ moduuli3/t7/7.js
@@ -0,0 +1,11 @@
+const trigger = document.getElementById('trigger');
+const target = document.getElementById('target');
+
+trigger.addEventListener('mouseover', () => {
+    target.src = 'img/picB.jpg';
+});
+
+
+trigger.addEventListener('mouseout', () => {
+            target.src = 'img/picA.jpg';
+        });
